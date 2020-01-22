c72712b50d8b518808c283c1fe30f591c01c23be
holtpd <holt.patrick@gmail.com>
2020-01-22 02:33:29 +0000

    Support updating tasks

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -9,7 +9,16 @@ RSpec.describe TasksController, type: :controller do
       expect(response).to have_http_status :success
       response_value = ActiveSupport::JSON.decode(@response.body)
       expect(response_value.count).to eq(2)
-
     end
   end
+    describe "task#update" do
+      it "should allow tasks to be marked as done" do
+        task = FactoryBot.create(:task, done: false)
+        put :update, params: {id: task.id, task: { done: true }}
+        expect(response).to have_http_status(:success)
+        task.reload
+        expect(task.done).to eq(true)
+      end
+    end
+
 end
